c996702ac0df2f5687b376808ad7a2f51bcb8472
Rúnar <ras89@hi.is>
2026-04-03 23:09:41 +0000

    added update score on review changed or added or removed

--- src/main/java/is/hi/hbv501g/mylib/Persistence/Repositories/ReviewRepository.java
+++ src/main/java/is/hi/hbv501g/mylib/Persistence/Repositories/ReviewRepository.java
@@ -2,6 +2,8 @@ package is.hi.hbv501g.mylib.Persistence.Repositories;
 
 import is.hi.hbv501g.mylib.Persistence.Entities.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 /******************************************************************************
  * @author Emma Ófeigsdóttir
@@ -14,4 +16,6 @@ public interface ReviewRepository extends JpaRepository<Review, Integer> {
 
     Review findReviewById(int id);
 
+    @Query("SELECT AVG(r.score) FROM Review r WHERE r.book.id = :bookId")
+Double getAverageScoreForBook(@Param("bookId") int bookId);
 }
